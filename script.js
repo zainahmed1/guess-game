596579adc8756b0a0100ca4383c13102f0c78482
zainahmed1 <50447349+zainahmed1@users.noreply.github.com>
2019-08-24 13:25:22 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let randomNumber = parseInt(Math.random() * 100);
 let trySelf = 0;
-let sound = new Audio("sound/joker.mp3");
+let sound = new Audio("sound/Aww Sound Effect.mp3");
+let sound2 = new Audio("sound/Kids Saying Yay [Sound Effect].mp3")
 function easyLevel() {
     let jokerNumber = document.querySelector("input").value;
     trySelf++;
@@ -11,16 +12,21 @@ function easyLevel() {
             sound.play();
             alert("Joker Says To Big");
         } else if (jokerNumber < randomNumber) {
+            sound.play();
             alert("You Coming Soon To Beat Joker Score");
         }
         else if (jokerNumber == randomNumber) {
-            alert(" Joker Says Congratulation Finally You Guess  Your Right " + trySelf + "times");
+            sound2.play();
+            alert(" Joker Says Congratulation Finally You Guess  Your Right " + trySelf + " times");
+            
         }
     }
 }
 
 let mediumRandomNumber = parseInt(Math.random() * 200);
 let mediumTrySelf = 0;
+let sound3 = new Audio("sound/wah wah sound effect.mp3");
+let sound4 = new Audio("sound/Audience Clapping - Sound Effect.mp3")
 function mediumLevel() {
     let jokerNumber = document.querySelector("input").value;
     mediumTrySelf++;
@@ -29,17 +35,22 @@ function mediumLevel() {
 
     } else {
         if (jokerNumber > mediumRandomNumber) {
+            sound3.play();
             alert("Joker Says To Big");
         } else if (jokerNumber < mediumRandomNumber) {
+            sound3.play();
             alert(" You Coming Soon To Beat Joker Score");
         }
         else if (jokerNumber == mediumRandomNumber) {
-            alert(" Congratulation Finally You Beat Joker" + mediumTrySelf + "times");
+            sound4.play();
+            alert(" Congratulation Finally You Beat Joker " + mediumTrySelf + " times");
         }
     }
 }
 let hardRandomNumber = parseInt(Math.random() * 420);
 let hardTrySelf = 0
+let sound5 = new Audio("sound/Female Evil Laugh Sound Effect.mp3");
+let sound6 = new Audio("sound/Gun reload and shot soundmost of the youtubers use.mp3")
 function hardLevel() {
     let jokerNumber = document.querySelector("input").value;
     hardTrySelf++;
@@ -47,12 +58,15 @@ function hardLevel() {
         alert("Joker Says Enter Your Number");
     } else {
         if (jokerNumber > hardRandomNumber) {
+            sound5.play();
             alert("Joker Says To Big");
         } else if (jokerNumber < hardRandomNumber) {
+            sound5.play();
             alert("You Coming Soon To Beat Joker Score");
         }
         else if (jokerNumber == hardRandomNumber) {
-            alert("  Congratulation Finally You Guess  Your Right " + hardTrySelf + "times");
+            sound6.play();
+            alert("  Congratulation Finally You Guess  Your Right " + hardTrySelf + " times");
         }
     }
 }
